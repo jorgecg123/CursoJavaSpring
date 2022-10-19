99828f1a5c5fa7c40ce63fab3e50b4310fe85dcb
JORGE LUIS CRUZ GARCIA <983448@horusur.corp>
2022-10-19 18:08:22 -0500

    first commit

---
+++ springbot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbotServicioItemApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotServicioItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotServicioItemApplication.class, args);
+	}
+
+}
